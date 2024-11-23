946149f27a5f8b28eb3b9af2c999b4fdc513cb06
Dineth Fernando <156776845+DinethSheveen@users.noreply.github.com>
2024-11-23 22:49:07 +0000

    Update Magazine.java

--- Magazine.java
+++ Magazine.java
@@ -9,34 +9,42 @@ package librarycentre_package;
  * @author User
  */
 public class Magazine extends Item{
-    
     private String issueNumber;
     private int publicationFrequency;
+    private String editor;
     
-    public Magazine(String title, String ISBN) {
-        super(title, ISBN);
+    public Magazine(String title,String ISBN){
+        super(title,ISBN);
     }
     
-    //Intance methods
-    public String getIssueNubmer(){
+    //Setter and getters
+    public String getIssueNumber(){
         return this.issueNumber;
+    }
+    
+     public String getEditor(){
+        return this.editor;
     }
     
     public int getPublicationFrequency(){
         return this.publicationFrequency;
     }
-       
+   
     public void setIssueNumber(String issueNumber){
-        this.issueNumber = issueNumber;
+        this.issueNumber=issueNumber;
     }
     
-    public void setPublicationFrequency(int publicationFrequency){
+    public void setEditor(String editor){
+        this.editor=editor;
+    }
+    
+    public void setPulicationFrequency(int publicationFrequency){
         this.publicationFrequency = publicationFrequency;
     }
     
-    
     @Override
     public String toString(){
-        return super.toString()+", Publications : "+this.publicationFrequency+", Issue Number : "+this.issueNumber;
+        return super.toString()+", Issue Number = "+this.getIssueNumber()+", Editor = "+this.getEditor()+", Publicatioon Frequency = "+this.getPublicationFrequency();
     }
+    
 }
